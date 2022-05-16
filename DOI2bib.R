4d883073f61dd02ee9cae0b608dfc87e12ba6e41
Emmanuel Paradis <emmanuelparadis@users.noreply.github.com>
2022-05-16 17:07:19 +0700

    Create DOI2bib.R

---
+++ DOI2bib.R
@@ -0,0 +1,25 @@
+## 1. input the DOIs, for instance:
+DOI <- read.table("....")$DOI
+DOI <- scan(what = "") # by hand or copy/paste
+## ... or else
+
+## 2. Give a name for the output file:
+OUTFILE <- "outfile.bib"
+
+## 3. Run the followings:
+baseurl <- "https://dx.doi.org/"
+hdr <- "application/x-bibtex"
+names(hdr) <- "Accept"
+
+for (doi in DOI) {
+    theurl <- paste0(baseurl, doi)
+    URL <- url(theurl, headers = hdr)
+    X <- try(scan(URL, "", sep = "\n"))
+    close(URL)
+    if (inherits(X, "try-error")) {
+        cat("==>> Download failed for doi =", doi, "<<==\n")
+        next
+    }
+    cat(X, sep = "\n", file = OUTFILE, append = TRUE)
+    cat("\n", file = OUTFILE, append = TRUE)
+}
